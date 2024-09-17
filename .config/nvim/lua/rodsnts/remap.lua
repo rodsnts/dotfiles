f276393dfa24cc503c0f4efa201ee78c5c1c04f7
Rodrigo Santos <rodrigo.santos@tradenation.com>
2024-09-17 09:09:54 +0100

    add prefer

--- .config/nvim/lua/rodsnts/remap.lua
+++ .config/nvim/lua/rodsnts/remap.lua
@@ -1,5 +1,7 @@
 vim.g.mapleader = " "
 
+vim.api.nvim_set_keymap("n", "gd", "<cmd>Telescope lsp_definitions<CR>", {})
+
 vim.api.nvim_set_keymap('t', '<esc><esc>', '<c-\\><c-n>', {})
 vim.keymap.set('n', "<leader>ds", function() require("noice").cmd("dismiss") end)
 -- Remap system yank to Leader + y
